212e79a0bb1a6a31998f287c218167b521b6ff80
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-09 22:43:18 +0330

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from chitti!'
+    message: 'Zirak BackEnd Service is Ready to Work :)'
   })
 })
 
